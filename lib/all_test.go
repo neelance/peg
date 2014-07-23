6ef780d59e2a1dd6fc65390765b8c789aa0f622f
Richard Musiol <mail@richard-musiol.de>
2014-07-23 14:29:04 +0200

    allow testing for label results

--- lib/all_test.go
+++ lib/all_test.go
@@ -1,6 +1,7 @@
 package peglib
 
 import (
+	"bytes"
 	"code.google.com/p/go.tools/go/loader"
 	"code.google.com/p/go.tools/go/ssa"
 	"code.google.com/p/go.tools/go/ssa/interp"
@@ -13,174 +14,174 @@ import (
 )
 
 func TestStringTerminal(t *testing.T) {
-	testRule(t, `'abc'`, map[string]bool{
-		"abc":  true,
-		"ab":   false,
-		"Abc":  false,
-		"abC":  false,
-		"abcX": false,
+	testRule(t, `'abc'`, map[string]string{
+		"abc":  "true",
+		"ab":   "false",
+		"Abc":  "false",
+		"abC":  "false",
+		"abcX": "false",
 	})
 
-	testRule(t, `"abc"`, map[string]bool{
-		"abc":  true,
-		"Abc":  true,
-		"abC":  true,
-		"ab":   false,
-		"Xbc":  false,
-		"abX":  false,
-		"abcX": false,
+	testRule(t, `"abc"`, map[string]string{
+		"abc":  "true",
+		"Abc":  "true",
+		"abC":  "true",
+		"ab":   "false",
+		"Xbc":  "false",
+		"abX":  "false",
+		"abcX": "false",
 	})
 }
 
 func TestCharacterClassTerminal(t *testing.T) {
-	testRule(t, `[b-df\-h]`, map[string]bool{
-		"b": true,
-		"c": true,
-		"d": true,
-		"f": true,
-		"-": true,
-		"h": true,
-		"a": false,
-		"e": false,
-		"g": false,
+	testRule(t, `[b-df\-h]`, map[string]string{
+		"b": "true",
+		"c": "true",
+		"d": "true",
+		"f": "true",
+		"-": "true",
+		"h": "true",
+		"a": "false",
+		"e": "false",
+		"g": "false",
 	})
 
-	testRule(t, `[^a]`, map[string]bool{
-		"b": true,
-		"a": false,
+	testRule(t, `[^a]`, map[string]string{
+		"b": "true",
+		"a": "false",
 	})
 
-	testRule(t, `[\n]`, map[string]bool{
-		"\n": true,
-		"n":  false,
+	testRule(t, `[\n]`, map[string]string{
+		"\n": "true",
+		"n":  "false",
 	})
 }
 
 func TestAnyCharacterTerminal(t *testing.T) {
-	testRule(t, `.`, map[string]bool{
-		"a":  true,
-		"B":  true,
-		"5":  true,
-		"":   false,
-		"99": false,
+	testRule(t, `.`, map[string]string{
+		"a":  "true",
+		"B":  "true",
+		"5":  "true",
+		"":   "false",
+		"99": "false",
 	})
 
-	testRule(t, `.*`, map[string]bool{
-		"aaa": true,
+	testRule(t, `.*`, map[string]string{
+		"aaa": "true",
 	})
 }
 
 func TestSequence(t *testing.T) {
-	testRule(t, `'abc' 'def'`, map[string]bool{
-		"abcdef":  true,
-		"abcde":   false,
-		"aXcdef":  false,
-		"abcdXf":  false,
-		"abcdefX": false,
+	testRule(t, `'abc' 'def'`, map[string]string{
+		"abcdef":  "true",
+		"abcde":   "false",
+		"aXcdef":  "false",
+		"abcdXf":  "false",
+		"abcdefX": "false",
 	})
 }
 
 func TestChoice(t *testing.T) {
-	testRule(t, `/ 'abc' / 'def'`, map[string]bool{
-		"abc":  true,
-		"def":  true,
-		"ab":   false,
-		"aXc":  false,
-		"defX": false,
+	testRule(t, `/ 'abc' / 'def'`, map[string]string{
+		"abc":  "true",
+		"def":  "true",
+		"ab":   "false",
+		"aXc":  "false",
+		"defX": "false",
 	})
 }
 
 func TestOptional(t *testing.T) {
-	testRule(t, `'abc'? 'def'`, map[string]bool{
-		"abcdef": true,
-		"def":    true,
-		"abc":    false,
-		"aXcdef": false,
-		"abdef":  false,
+	testRule(t, `'abc'? 'def'`, map[string]string{
+		"abcdef": "true",
+		"def":    "true",
+		"abc":    "false",
+		"aXcdef": "false",
+		"abdef":  "false",
 	})
 }
 
 func TestZeroOrMore(t *testing.T) {
-	testRule(t, `'a'*`, map[string]bool{
-		"":      true,
-		"a":     true,
-		"aaaaa": true,
-		"X":     false,
-		"aaaX":  false,
+	testRule(t, `'a'*`, map[string]string{
+		"":      "true",
+		"a":     "true",
+		"aaaaa": "true",
+		"X":     "false",
+		"aaaX":  "false",
 	})
 }
 
 func TestOneOrMore(t *testing.T) {
-	testRule(t, `'a'+`, map[string]bool{
-		"a":     true,
-		"aaaaa": true,
-		"":      false,
-		"X":     false,
-		"aaaX":  false,
+	testRule(t, `'a'+`, map[string]string{
+		"a":     "true",
+		"aaaaa": "true",
+		"":      "false",
+		"X":     "false",
+		"aaaX":  "false",
 	})
 }
 
 func TestRepetitionGlue(t *testing.T) {
-	testRule(t, `'a'*[ ',' ]`, map[string]bool{
-		"":      true,
-		"a":     true,
-		"a,a,a": true,
-		"aa":    false,
-		",":     false,
-		"a,a,":  false,
-		",a,a":  false,
-		"a,,a":  false,
+	testRule(t, `'a'*[ ',' ]`, map[string]string{
+		"":      "true",
+		"a":     "true",
+		"a,a,a": "true",
+		"aa":    "false",
+		",":     "false",
+		"a,a,":  "false",
+		",a,a":  "false",
+		"a,,a":  "false",
 	})
 
-	testRule(t, `'a'+[ ',' ]`, map[string]bool{
-		"a":     true,
-		"a,a,a": true,
-		"aa":    false,
-		"":      false,
-		",":     false,
-		"a,a,":  false,
-		",a,a":  false,
-		"a,,a":  false,
+	testRule(t, `'a'+[ ',' ]`, map[string]string{
+		"a":     "true",
+		"a,a,a": "true",
+		"aa":    "false",
+		"":      "false",
+		",":     "false",
+		"a,a,":  "false",
+		",a,a":  "false",
+		"a,,a":  "false",
 	})
 }
 
 func TestUntil(t *testing.T) {
-	testRule(t, `( 'a' . )*->'ac'`, map[string]bool{
-		"ac":       true,
-		"ababac":   true,
-		"":         false,
-		"ab":       false,
-		"abXbac":   false,
-		"ababacX":  false,
-		"ababacab": false,
-		"ababacac": false,
+	testRule(t, `( 'a' . )*->'ac'`, map[string]string{
+		"ac":       "true",
+		"ababac":   "true",
+		"":         "false",
+		"ab":       "false",
+		"abXbac":   "false",
+		"ababacX":  "false",
+		"ababacab": "false",
+		"ababacac": "false",
 	})
 }
 
 func TestParenthesizedExpression(t *testing.T) {
-	testRule(t, `( 'a' ( ) 'b' )? 'c'`, map[string]bool{
-		"abc": true,
-		"c":   true,
-		"ac":  false,
-		"bc":  false,
+	testRule(t, `( 'a' ( ) 'b' )? 'c'`, map[string]string{
+		"abc": "true",
+		"c":   "true",
+		"ac":  "false",
+		"bc":  "false",
 	})
 }
 
 func TestPositiveLookahead(t *testing.T) {
-	testRule(t, `&'a' .`, map[string]bool{
-		"a":  true,
-		"":   false,
-		"X":  false,
-		"aX": false,
+	testRule(t, `&'a' .`, map[string]string{
+		"a":  "true",
+		"":   "false",
+		"X":  "false",
+		"aX": "false",
 	})
 }
 
 func TestNegativeLookahead(t *testing.T) {
-	testRule(t, `!'a' .`, map[string]bool{
-		"X":  true,
-		"":   false,
-		"a":  false,
-		"XX": false,
+	testRule(t, `!'a' .`, map[string]string{
+		"X":  "true",
+		"":   "false",
+		"a":  "false",
+		"XX": "false",
 	})
 }
 
@@ -189,9 +190,9 @@ func TestRuleDefinition(t *testing.T) {
 		rule SomeName
 			'a'
 		end
-	`, "SomeName", map[string]bool{
-		"a": true,
-		"X": false,
+	`, "SomeName", map[string]string{
+		"a": "true",
+		"X": "false",
 	})
 }
 
@@ -203,10 +204,10 @@ func TestRuleReference(t *testing.T) {
     rule a
       'b'
     end
-	`, "Test", map[string]bool{
-		"b": true,
-		"X": false,
-		"a": false,
+	`, "Test", map[string]string{
+		"b": "true",
+		"X": "false",
+		"a": "false",
 	})
 }
 
@@ -215,12 +216,12 @@ func TestRecursiveRule(t *testing.T) {
     rule Test
       '(' Test ')' / ( )
     end
-	`, "Test", map[string]bool{
-		"":       true,
-		"()":     true,
-		"((()))": true,
-		"()))":   false,
-		"((()":   false,
+	`, "Test", map[string]string{
+		"":       "true",
+		"()":     "true",
+		"((()))": "true",
+		"()))":   "false",
+		"((()":   "false",
 	})
 }
 
@@ -234,24 +235,24 @@ func TestRecursiveRule(t *testing.T) {
 // 	//     assert "b" === result[:char]
 
 // 	testRule(t, `word:( 'a' 'b' 'c' )`, map[string]bool {
-// 	"abc") == { word: "abc" : true,
+// 	"abc") == { word: "abc" : "true",
 
 // 	testRule(t, `( word:[abc]+ )?`, map[string]bool {
-// 	"abc") == { word: "abc" : true,
-// 	"") == {: true,
+// 	"abc") == { word: "abc" : "true",
+// 	"") == {: "true",
 
 // 	testRule(t, `'a' outer:( inner:. ) 'c' / 'def'`, map[string]bool {
-// 	"abc") == { outer: { inner: "b" } : true,
+// 	"abc") == { outer: { inner: "b" } : "true",
 // }
 
 // func TestNestedLabel(t *testing.T) {
 // 	testRule(t, `word:( 'a' char:. 'c' )`, map[string]bool {
-// 	"abc") == { word: { char: "b" } : true,
+// 	"abc") == { word: { char: "b" } : "true",
 // }
 
 // func TestAtLabel(t *testing.T) {
 // 	testRule(t, `'a' @:. 'c'`, map[string]bool {
-// 	"abc") == "b: true,
+// 	"abc") == "b: "true",
 
 // 	testGrammar(t, `
 // 	//       rule test
@@ -266,9 +267,9 @@ func TestRecursiveRule(t *testing.T) {
 
 // func TestLabelMerge(t *testing.T) {
 // 	testRule(t, `( char:'a' x:'x' / 'b' x:'x' / char:( inner:'c' ) x:'x' ) / 'y'`, map[string]bool {
-// 	"ax") == { char: "a", x: "x" : true,
-// 	"bx") == { x: "x" : true,
-// 	"cx") == { char: { inner: "c" }, x: "x" : true,
+// 	"ax") == { char: "a", x: "x" : "true",
+// 	"bx") == { x: "x" : "true",
+// 	"cx") == { char: { inner: "c" }, x: "x" : "true",
 // }
 
 // func TestRuleWithLabel(t *testing.T) {
@@ -311,16 +312,16 @@ func TestRecursiveRule(t *testing.T) {
 
 // func TestRepetitionWithLabel(t *testing.T) {
 // 	testRule(t, `list:( char:( 'a' / 'b' / 'c' ) )*`, map[string]bool {
-// 	"abc") == { list: [{ char: "a" }, { char: "b" }, { char: "c" }] : true,
+// 	"abc") == { list: [{ char: "a" }, { char: "b" }, { char: "c" }] : "true",
 
 // 	testRule(t, `list:( char:'a' / char:'b' / 'c' )+`, map[string]bool {
-// 	"abc") == { list: [{ char: "a" }, { char: "b" }, {}] : true,
+// 	"abc") == { list: [{ char: "a" }, { char: "b" }, {}] : "true",
 
 // 	testRule(t, `( 'a' / 'b' / 'c' )+`, map[string]bool {
-// 	"abc") == {: true,
+// 	"abc") == {: "true",
 
 // 	testRule(t, `list:( 'a' char:. )*->( 'ada' final:. )`, map[string]bool {
-// 	"abacadae") == { list: [{ char: "b" }, { char: "c" }, { final: "e" }] : true,
+// 	"abacadae") == { list: [{ char: "b" }, { char: "c" }, { final: "e" }] : "true",
 
 // 	testGrammar(t, `
 // 	//       rule test
@@ -332,11 +333,11 @@ func TestRecursiveRule(t *testing.T) {
 
 // func TestObjectCreator(t *testing.T) {
 // 	testRule(t, `'a' char:. 'c' <TestClassA> / 'd' char:. 'f' <TestClassB>", class_scope: self.clas`, map[string]bool {
-// 	"abc") == TestClassA.new({ char: "b" }: true,
-// 	"def") == TestClassB.new({ char: "e" }: true,
+// 	"abc") == TestClassA.new({ char: "b" }: "true",
+// 	"def") == TestClassB.new({ char: "e" }: "true",
 
-// 	testRule(t, `'a' char:. 'c' <TestClassA { a: 'test1', b: [ <TestClassB true>, <TestClassB { r: @char }> ] }>", class_scope: self.clas`, map[string]bool {
-// 	"abc") == TestClassA.new({ a: "test1", b: [ TestClassB.new(true), TestClassB.new({ r: "b" }) ] }: true,
+// 	testRule(t, `'a' char:. 'c' <TestClassA { a: 'test1', b: [ <TestClassB "true">, <TestClassB { r: @char }> ] }>", class_scope: self.clas`, map[string]bool {
+// 	"abc") == TestClassA.new({ a: "test1", b: [ TestClassB.new("true"), TestClassB.new({ r: "b" }) ] }: "true",
 // }
 
 // func TestValueCreator(t *testing.T) {
@@ -345,30 +346,30 @@ func TestRecursiveRule(t *testing.T) {
 // 	//       word:'def' { @word.chars.map { |c| c.ord } } /
 // 	//       'ghi' { [__FILE__, __LINE__] }
 // 	//     ", filename: "test.jetpeg"
-// 	"abc") == "B: true,
-// 	"def") == ["d".ord, "e".ord, "f".ord: true,
-// 	"ghi") == ["test.jetpeg", 4: true,
+// 	"abc") == "B: "true",
+// 	"def") == ["d".ord, "e".ord, "f".ord: "true",
+// 	"ghi") == ["test.jetpeg", 4: "true",
 // }
 
 // func TestLocalLabel(t *testing.T) {
 // 	testRule(t, `'a' %temp:( char:'b' )* 'c' ( result:%temp )`, map[string]bool {
-// 	"abc") == { result: [{ char: "b" }] : true,
-// 	"abX") == ni: true,
+// 	"abc") == { result: [{ char: "b" }] : "true",
+// 	"abX") == ni: "true",
 
 // 	testRule(t, `'a' %temp:( char:'b' )* 'c' result1:%temp result2:%temp`, map[string]bool {
-// 	"abc") == { result1: [{ char: "b" }], result2: [{ char: "b" }] : true,
+// 	"abc") == { result1: [{ char: "b" }], result2: [{ char: "b" }] : "true",
 // }
 
 // func TestParameters(t *testing.T) {
 // 	testGrammar(t, `
 // 	//       rule test
-// 	//         %a:. %b:. test2[%a, %b, $true]
+// 	//         %a:. %b:. test2[%a, %b, $"true"]
 // 	//       end
 // 	//       rule test2[%v, %w, %x]
 // 	//         result1:%v result2:%w result3:%x
 // 	//       end
 // 	//     "
-// 	//     assert grammar.parse_rule(:test, "ab") == { result1: "a", result2: "b", result3: true }
+// 	//     assert grammar.parse_rule(:test, "ab") == { result1: "a", result2: "b", result3: "true" }
 // }
 
 // func TestUndefinedLocalLabelError(t *testing.T) {
@@ -395,20 +396,20 @@ func TestRecursiveRule(t *testing.T) {
 // }
 
 // func TestBooleanFunctions(t *testing.T) {
-// 	testRule(t, `'a' v:$true 'bc' / 'd' v:$false 'ef'`, map[string]bool {
-// 	"abc") == { v: true : true,
-// 	"def") == { v: false : true,
+// 	testRule(t, `'a' v:$"true" 'bc' / 'd' v:$"false 'ef'`, map[string]bool "{
+// 	"abc") == { v: "true" : "true",
+// 	"def") == { v: "false : "true"",
 //   })
 
-// 	testRule(t, `'a' ( 'b' v:$true )? 'c'`, map[string]bool {
-// 	"abc") == { v: true : true,
-// 	"ac") == {: true,
+// 	testRule(t, `'a' ( 'b' v:$"true" )? 'c'`, map[string]bool {
+// 	"abc") == { v: "true" : "true",
+// 	"ac") == {: "true",
 //   })
 // }
 
 // func TestErrorFunction(t *testing.T) {
 // 	testRule(t, `'a' $error['test'] 'bc'`, map[string]bool {
-// 	"abc": false,
+// 	"abc": "false",
 // 	//     assert rule.parser.failure_reason.is_a? JetPEG::ParsingError
 // 	//     assert rule.parser.failure_reason.position == 1
 // 	//     assert rule.parser.failure_reason.other_reasons == ["test"]
@@ -416,12 +417,12 @@ func TestRecursiveRule(t *testing.T) {
 
 // func TestMatchFunction(t *testing.T) {
 // 	testRule(t, `%a:( . . ) $match[%a]`, map[string]bool {
-// 	"abab": true,
-// 	"cdcd": true,
-// 	"a": false,
-// 	"ab": false,
-// 	"aba": false,
-// 	"abaX": false,
+// 	"abab": "true",
+// 	"cdcd": "true",
+// 	"a": "false",
+// 	"ab": "false",
+// 	"aba": "false",
+// 	"abaX": "false",
 // }
 
 // func TestModes(t *testing.T) {
@@ -439,11 +440,11 @@ func TestRecursiveRule(t *testing.T) {
 // 	//     assert !grammar.parse_rule(:test, "abb")
 // }
 
-func testRule(t *testing.T, rule string, inputs map[string]bool) {
+func testRule(t *testing.T, rule string, inputs map[string]string) {
 	testGrammar(t, "rule Test\n"+rule+"\nend\n", "Test", inputs)
 }
 
-func testGrammar(t *testing.T, grammar, mainRule string, inputs map[string]bool) {
+func testGrammar(t *testing.T, grammar, mainRule string, inputs map[string]string) {
 	fset := token.NewFileSet()
 	file := Compile(grammar, mainRule, fset)
 
@@ -472,19 +473,16 @@ func testGrammar(t *testing.T, grammar, mainRule string, inputs map[string]bool)
 	prog := ssa.Create(iprog, ssa.SanityCheckFunctions)
 	prog.BuildAll()
 
-	for input, matchExpected := range inputs {
-		exitCode := interp.Interpret(prog.Package(iprog.Created[0].Pkg), 0, config.TypeChecker.Sizes, "main.go", []string{input})
-		switch exitCode {
-		case 100:
-			if !matchExpected {
-				t.Errorf("grammar %q did match %q", grammar, input)
-			}
-		case 101:
-			if matchExpected {
-				t.Errorf("grammar %q did not match %q", grammar, input)
-			}
-		default:
-			t.Errorf("unexpected exit code: %d", exitCode)
+	for input, expected := range inputs {
+		interp.CapturedOutput = bytes.NewBuffer(nil)
+		if exitCode := interp.Interpret(prog.Package(iprog.Created[0].Pkg), 0, config.TypeChecker.Sizes, "main.go", []string{input}); exitCode != 0 {
+			t.Errorf("exit code: %d", exitCode)
+			continue
+		}
+		expected += "\n"
+		got := interp.CapturedOutput.String()
+		if expected != got {
+			t.Errorf("grammar %q gave wrong result on %q: expected %q, got %q", grammar, input, expected, got)
 		}
 	}
 }
--- runtime/runtime.go
+++ runtime/runtime.go
@@ -2,16 +2,80 @@ package pegruntime
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"strings"
 )
 
+type Stringer interface {
+	String() string
+}
+
+type InputRange struct {
+	Input []byte
+	Start int
+	End   int
+}
+
+func (r *InputRange) Bytes() []byte {
+	return r.Input[r.Start:r.End]
+}
+
+func (r *InputRange) String() string {
+	return string(r.Bytes())
+}
+
+type StringData string
+
+func (s StringData) String() string {
+	return string(s)
+}
+
+type ParsingError struct {
+	Input        []byte
+	Position     int
+	Expectations []string
+	OtherReasons []string
+}
+
+func (e *ParsingError) Error() string {
+	before := e.Input[:e.Position]
+	line := bytes.Count(before, []byte{'\n'}) + 1
+	column := len(before) - bytes.LastIndex(before, []byte{'\n'})
+	if column == len(before)+1 {
+		column = len(before)
+	}
+
+	reasons := e.OtherReasons
+	if len(e.Expectations) != 0 {
+		reasons = append(reasons, "expected one of "+strings.Join(e.Expectations, ", "))
+	}
+	prefixOffset := len(before) - 20
+	if prefixOffset < 0 {
+		prefixOffset = 0
+	}
+	return fmt.Sprintf("at line %d, column %d (byte %d, after %q): %s", line, column, e.Position, string(before[prefixOffset:]), strings.Join(reasons, " / "))
+}
+
+// callbacks rely on global variables since Go has no easy way of passing closures to C and the JetPEG backend has no support for passing the parser object yet
+var Debug = false
+var Factory = func(class string, value interface{}) interface{} { return value }
+var input []byte
+var inputOffset uintptr
+var outputStack []interface{}
+var localsStack []interface{}
+var tempSource map[string]interface{}
+var failurePosition int
+var failureExpectations []string
+var failureOtherReasons []string
+
 func Main(rule func([]byte) []byte) {
 	inputAtEnd := rule(append([]byte(os.Args[1]), 0))
 	if len(inputAtEnd) != 1 || inputAtEnd[0] != 0 {
-		os.Exit(101)
+		fmt.Println("false")
+		return
 	}
-	os.Exit(100)
+	fmt.Println("true")
 }
 
 func HasPrefix(input []byte, prefix string) bool {
@@ -24,4 +88,193 @@ func HasPrefixFold(input []byte, prefix string) bool {
 
 func ContainsByte(s string, b byte) bool {
 	return strings.IndexByte(s, b) != -1
+}
+
+func pushOutput(v interface{}) {
+	outputStack = append(outputStack, v)
+}
+
+func popOutput() interface{} {
+	v := outputStack[len(outputStack)-1]
+	outputStack = outputStack[:len(outputStack)-1]
+	return v
+}
+
+func PushEmpty() {
+	if Debug {
+		fmt.Printf("pushEmpty()\n")
+	}
+	outputStack = append(outputStack, make(map[string]interface{}))
+}
+
+func PushInputRange(from uintptr, to uintptr) {
+	if Debug {
+		fmt.Printf("pushInputRange(%d, %d)\n", from-inputOffset, to-inputOffset)
+	}
+	pushOutput(&InputRange{input, int(from - inputOffset), int(to - inputOffset)})
+}
+
+func PushBoolean(value bool) {
+	if Debug {
+		fmt.Printf("pushBoolean(%t)\n", value)
+	}
+	pushOutput(value)
+}
+
+func PushString(value string) {
+	if Debug {
+		fmt.Printf("pushString(%q)\n", value)
+	}
+	pushOutput(StringData(value))
+}
+
+func PushArray(appendCurrent bool) {
+	if Debug {
+		fmt.Printf("pushArray(%t)\n", appendCurrent)
+	}
+	if appendCurrent {
+		pushOutput([]interface{}{popOutput()})
+		return
+	}
+	pushOutput([]interface{}{})
+}
+
+func AppendToArray() {
+	if Debug {
+		fmt.Printf("appendToArray()\n")
+	}
+	v := popOutput()
+	pushOutput(append(popOutput().([]interface{}), v))
+}
+
+func MakeLabel(name string) {
+	if Debug {
+		fmt.Printf("makeLabel(%q)\n", name)
+	}
+	pushOutput(map[string]interface{}{name: popOutput()})
+}
+
+func MergeLabels(count int) {
+	if Debug {
+		fmt.Printf("mergeLabels(%d)\n", count)
+	}
+	merged := make(map[string]interface{})
+	for i := 0; i < int(count); i++ {
+		if m, ok := popOutput().(map[string]interface{}); ok {
+			for k, v := range m {
+				merged[k] = v
+			}
+		}
+	}
+	pushOutput(merged)
+}
+
+func MakeValue(code string, filename string, line int) {
+	if Debug {
+		fmt.Printf("makeValue(%q, %q, %d)\n", code, filename, line)
+	}
+	panic("makeValue not supported")
+}
+
+func MakeObject(class string) {
+	if Debug {
+		fmt.Printf("makeObject(%q)\n", class)
+	}
+	pushOutput(Factory(class, popOutput()))
+}
+
+func Pop() {
+	if Debug {
+		fmt.Printf("pop()\n")
+	}
+	popOutput()
+}
+
+func LocalsPush(count int) {
+	if Debug {
+		fmt.Printf("localsPush(%d)\n", count)
+	}
+	for i := 0; i < int(count); i++ {
+		localsStack = append(localsStack, popOutput())
+	}
+}
+
+func LocalsLoad(index int) {
+	if Debug {
+		fmt.Printf("localsLoad(%d)\n", index)
+	}
+	pushOutput(localsStack[len(localsStack)-1-int(index)])
+}
+
+func LocalsPop(count int) {
+	if Debug {
+		fmt.Printf("localsPop(%d)\n", count)
+	}
+	localsStack = localsStack[:len(localsStack)-int(count)]
+}
+
+func Match(absPos uintptr) uintptr {
+	pos := absPos - inputOffset
+	if Debug {
+		fmt.Printf("match(%d)\n", pos)
+	}
+	var expected []byte
+	switch e := popOutput().(type) {
+	case *InputRange:
+		expected = e.Bytes()
+	case StringData:
+		expected = []byte(e)
+	default:
+		panic("invalid type for match")
+	}
+	if bytes.HasPrefix(input[pos:], expected) {
+		return absPos + uintptr(len(expected))
+	}
+	return 0
+}
+
+func SetAsSource() {
+	if Debug {
+		fmt.Printf("setAsSource()\n")
+	}
+	tempSource, _ = popOutput().(map[string]interface{})
+}
+
+func ReadFromSource(name string) {
+	if Debug {
+		fmt.Printf("readFromSource(%q)\n", name)
+	}
+	pushOutput(tempSource[name])
+}
+
+func TraceEnter(name string) {
+	if Debug {
+		fmt.Printf("traceEnter(%q)\n", name)
+	}
+}
+
+func TraceLeave(name string, successful bool) {
+	if Debug {
+		fmt.Printf("traceLeave(%q, %t)\n", name, successful)
+	}
+}
+
+func TraceFailure(absPos uintptr, reason string, isExpectation bool) {
+	pos := int(absPos - inputOffset)
+	if Debug {
+		fmt.Printf("traceFailure(%d, %q, %t  )\n", pos, reason, isExpectation)
+	}
+	if pos > failurePosition {
+		failurePosition = pos
+		failureExpectations = nil
+		failureOtherReasons = nil
+	}
+	if pos == failurePosition {
+		switch isExpectation {
+		case true:
+			failureExpectations = append(failureExpectations, reason)
+		case false:
+			failureOtherReasons = append(failureOtherReasons, reason)
+		}
+	}
 }
